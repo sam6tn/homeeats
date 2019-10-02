9154e384be94546f2de2c4db2e479247c94f0852
guillermo saavedra <gs8rj@virginia.edu>
2019-10-02 12:02:21 -0400

    Adds Cook and Address model, Adds app to settings.py

--- app/homeeats/homeeats_app/models.py
+++ app/homeeats/homeeats_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Cook(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  email = models.CharField(max_length=30)
+  password = models.CharField(max_length=30)
+  address = models.OneToOneField(
+    Address,
+    on_delete=models.CASCADE,
+    primary_key=True,
+  )
+  approved = models.BooleanField(default=False)
+  kitchen_license = models.CharField(max_length=30)
+  cuisines = ArrayField(
+            models.CharField(max_length=10, blank=True),
+          )
+
+class Address(models.Mode):
+  street_address = models.CharField(max_length=60) 
