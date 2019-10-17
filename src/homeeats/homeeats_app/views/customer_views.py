209321b79453be480242a7845cf4d4fdb755aff2
Isabel Kershner <ik7qk@virginia.edu>
2019-10-16 21:42:36 -0400

    Edited url and customer view

--- src/homeeats/homeeats_app/views/customer_views.py
+++ src/homeeats/homeeats_app/views/customer_views.py
@@ -3,12 +3,16 @@ from ..forms import CustomerCreateForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
+def index(request):
+    template = loader.get_template('../templates/index.html')
+    return HttpResponse(template.render())
+
 def create(request):
   if request.method == 'POST':
     form = CustomerCreateForm(request.POST)
     if form.is_valid():
-        cook = form.save(commit=False)
-        cook.save()
+        customer = form.save(commit=False)
+        customer.save()
         return HttpResponseRedirect(reverse('customer_login'))
   else:
     form = CustomerCreateForm()
