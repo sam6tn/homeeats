a0f01c602ac3de49879f3244e044fafbe2460004
agy8gc <ankith.y@gmail.com>
2019-10-24 13:26:58 -0400

    cuisines for a cook show up on landing page

--- src/homeeats/homeeats_app/urls/cook_urls.py
+++ src/homeeats/homeeats_app/urls/cook_urls.py
@@ -2,6 +2,5 @@ from ..views import cook_views
 from django.urls import path
 
 urlpatterns = [
-  path('get/', cook_views.get, name='cook_get'),
   path('home/', cook_views.home, name='cook_home')
 ]
