8f4a2783e0ee6e829abcdd11a622ac6780ddf8ea
agy8gc <ankith.y@gmail.com>
2019-10-26 21:20:04 -0400

    travis

--- src/homeeats/homeeats_app/urls/main_urls.py
+++ src/homeeats/homeeats_app/urls/main_urls.py
@@ -1,11 +1,11 @@
-from ..views import main_views as main
+# from ..views import main_views as main
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path('', main.index, name='index'),
-  path('login', main.userLogin, name='login'),
-  path('logout', main.logout_view, name='logout'),
-  path('customercreate', main.customercreate, name='customercreate'),
-  path('cookcreate', main.cookcreate, name='cookcreate')
+  # path('login', main.userLogin, name='login'),
+  # path('logout', main.logout_view, name='logout'),
+  # path('customercreate', main.customercreate, name='customercreate'),
+  # path('cookcreate', main.cookcreate, name='cookcreate')
 ]
