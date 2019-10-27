092034f99d6fe27f0aa89c44931c3e7840e7f7a9
agy8gc <ankith.y@gmail.com>
2019-10-27 16:14:57 -0400

    5 unit tests added for sprint

--- src/homeeats/homeeats_app/tests.py
+++ src/homeeats/homeeats_app/tests.py
@@ -20,4 +20,22 @@ class CookHomeTest(TestCase):
         response = self.client.get(reverse('cook_home'))
         self.assertEquals(response.status_code, 302)
 
+class CustomerHomeTest(TestCase):
+    def test_not_logged_in_causes_redirect_to_login_for_cook_home(self):
+        response = self.client.get(reverse('customer_home'))
+        self.assertEquals(response.status_code, 302)
+
+class AccountCreationTest(TestCase):
+    def test_cook_create_with_valid_data(self):
+        response = self.client.post(reverse('cookcreate'), {'first_name': 'Bob', 'last_name': 'Saget', 'email': 'bob@bob.com', 'password': 'sagetsaget', 'kitchen_license': 'asdfasdfasdf', 'phone_number': '7777777777'})
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url, "/")
+    def test_customer_create_with_valid_data(self):
+        response = self.client.post(reverse('customercreate'), {'first_name': 'Dave', 'last_name': 'Chapelle', 'street': '221 Baker Street', 'town': 'Fairfax', 'state': 'VA', 'zipcode': '22000', 'email': 'dave@dave.com', 'password': 'chapchap', 'phone_number': '8888888888'})
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url, "/")
+    
+
+    
+
 
