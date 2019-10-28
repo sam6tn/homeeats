de2fe3ce0fbd8b2c01af9d1e3c2fb78b3d00957d
sam6tn <sam6tn@users.noreply.github.com>
2019-10-28 17:09:00 -0400

    Must be logged in as customer

--- src/homeeats/homeeats_app/views/customer_views.py
+++ src/homeeats/homeeats_app/views/customer_views.py
@@ -5,25 +5,32 @@ from ..forms import CustomerCreateForm
 from .. import forms
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from ..models import Dish
+from ..models import Dish, Customer
 from .. import models
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
 
 '''
 Homepage view before login
 '''
-def index(request):
-    template = loader.get_template('../templates/index.html')
-    return HttpResponse(template.render())
 
+@login_required
 def dish(request, dish_id):
+    try:
+      customer = Customer.objects.get(user_id=request.user.id)
+    except Exception as e:
+      return redirect('/')
     dish = Dish.objects.get(id=dish_id) #get Dish object from dish_id
     reviews = dish.dish_review_set.all() #get all reviews for that Dish
     return render(request, 'customer_templates/customer_dish.html', {'dish': dish, 'reviews':reviews})
 
 @login_required
 def home(request):
-    customer = get_object_or_404(models.Customer, user_id=request.user.id)
+    try:
+      customer = Customer.objects.get(user_id=request.user.id)
+    except Exception as e:
+      return redirect('/')
     if request.method == 'POST':
       form = forms.DishSearchForm(request.POST)
       if form.is_valid():
