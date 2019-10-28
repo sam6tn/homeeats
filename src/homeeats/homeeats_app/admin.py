340006920d4b5b9a701f56f567090029c87718ed
Habib Karaky <hkaraky1@gmail.com>
2019-10-27 23:51:40 -0400

    Update admin.py
    Customizing Admin Homepage

--- src/homeeats/homeeats_app/admin.py
+++ src/homeeats/homeeats_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import Cook, Customer, Dish, Dish_Review, Cuisine
 
+admin.site.site_header = "HomeEats Admin Page"
 # Register your models here.
-
+admin.site.unregister(Group)
 admin.site.register(Cook)
 admin.site.register(Customer)
 admin.site.register(Dish)
