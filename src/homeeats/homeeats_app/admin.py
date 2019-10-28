5f7f1d891078bb3bd62abaa45379f859beff6b67
Habib Karaky <hkaraky1@gmail.com>
2019-10-28 00:17:27 -0400

    Update admin.py
    Adding display and filters to Admin display for Dishes, Cooks, and Customers

--- src/homeeats/homeeats_app/admin.py
+++ src/homeeats/homeeats_app/admin.py
@@ -3,10 +3,23 @@ from django.contrib.auth.models import Group
 from .models import Cook, Customer, Dish, Dish_Review, Cuisine
 
 admin.site.site_header = "HomeEats Admin Page"
+
+#Customizing the display and possible filters when looking through each of these models from the admin page
+class DishAdmin(admin.ModelAdmin):
+	list_display = ('title', 'cuisine', 'cook')
+	list_filter = ('cusine',)
+
+class CookAdmin(admin.ModelAdmin):
+	list_display = ('user', 'approved', 'kitchenlicense')
+
+class CustomerAdmin(admin.ModelAdmin):
+	list_display = ('user', 'phone_number')
+
+
 # Register your models here.
 admin.site.unregister(Group)
-admin.site.register(Cook)
-admin.site.register(Customer)
-admin.site.register(Dish)
+admin.site.register(Cook, CookAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Dish, DishAdmin)
 admin.site.register(Dish_Review)
-admin.site.register(Cuisine)
+#admin.site.register(Cuisine)
