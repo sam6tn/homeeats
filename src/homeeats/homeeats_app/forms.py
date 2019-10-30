a86c6ccf1432f1d5de4abbbdfe499327147ab801
Jack Short <john.short.tj@gmail.com>
2019-10-30 13:40:15 -0400

    fixed cook signup

--- src/homeeats/homeeats_app/forms.py
+++ src/homeeats/homeeats_app/forms.py
@@ -1,11 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-from django.contrib.auth.models import User
 from django import forms
-from .models import Cuisine
-from .models import Customer
-from .models import Cook
-from .models import Dish
+from .models import User, Cuisine, Customer, Cook, Dish
 
 
 '''
