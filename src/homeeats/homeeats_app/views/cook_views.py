199f0ea57c4b0f7c11315a99cf376aabafc3f5b4
agy8gc <ankith.y@gmail.com>
2019-11-19 00:19:44 -0500

    cooks can now report reviews to their dishes

--- src/homeeats/homeeats_app/views/cook_views.py
+++ src/homeeats/homeeats_app/views/cook_views.py
@@ -200,9 +200,11 @@ def change_order_status(previous, new, request, order_id):
     order.status = new
     order.save()
 
+@login_required
+@cook_required
 def reviews_for_dish(request, dish_id):
   dish = get_object_or_404(Dish, id=dish_id)
-  objs = Dish_Review.objects.filter(dish=dish)
+  objs = Dish_Review.objects.filter(dish=dish, report_flag=False)
   reviews = []
   for obj in objs:
     reviews.append(model_to_dict(obj))
@@ -211,3 +213,13 @@ def reviews_for_dish(request, dish_id):
     'reviews': reviews
   }
   return render(request, 'cook_templates/cook_dish_reviews.html', context)
+
+@login_required
+@cook_required
+def report_dish_review(request, dish_review_id, reason):
+  dish_review = Dish_Review.objects.get(id=dish_review_id)
+  dish_review.report_flag=True
+  dish_review.report_reason=reason
+  dish_review.save()
+  messages.add_message(request, messages.ERROR, 'Dish review has been flagged to be further evaluated')
+  return HttpResponseRedirect(reverse('reviews_for_dish', args=[dish_review.dish_id]))
